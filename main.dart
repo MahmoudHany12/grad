8269e204b36051ea810bf88e33c68d583037ec4c
MahmoudHany12 <99086974+MahmoudHany12@users.noreply.github.com>
2024-12-06 01:32:16 +0200

    Update main.dart
    saving data of the user using firebase

--- main.dart
+++ main.dart
@@ -1,27 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'login_screen.dart';
 import 'home_screen.dart';
-import 'create_account_screen.dart'; // Import create_account_screen.dart
-import 'package:google_fonts/google_fonts.dart';
+import 'create_account_screen.dart';
+import 'splash_screen.dart';
 
-void main() {
-  runApp(HistoryApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Initialize Firebase only if not already initialized
+  if (Firebase.apps.isEmpty) {
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+        apiKey: "AIzaSyDIQ29Wrak9iYGK2C8xzTjQGYKaxW0U1-0",
+        appId: "1:419926934270:android:9dfb3c2d0ac64ad5f4dbaf",
+        messagingSenderId: "419926934270",
+        projectId: "historealm-80a25",
+        storageBucket: "historealm-80a25.firebasestorage.app",
+      ),
+    );
+  }
+
+  runApp( HistoryApp());
 }
 
 class HistoryApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'History Explorer',
+      title: 'HistoRealm',
       theme: ThemeData(
-        textTheme: GoogleFonts.cinzelTextTheme(), // Set Cinzel as the default font
         primarySwatch: Colors.brown,
       ),
-      initialRoute: '/',
+      initialRoute: '/splash',
       routes: {
+        '/splash': (context) => SplashScreen(),
         '/': (context) => LoginScreen(),
         '/home': (context) => HomeScreen(),
-        '/create-account': (context) => CreateAccountScreen(), // Add CreateAccountScreen route
+        '/create-account': (context) => CreateAccountScreen(),
       },
     );
   }
